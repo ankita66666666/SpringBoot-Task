d2188e63b6d78c55ddb382f883789980dc9200c3
Ankita <kumariankita.gaurav@cgi.com>
2019-08-02 21:39:44 +0530

    completed TrackByName task

--- src/main/java/com/stackroute/service/TrackService.java
+++ src/main/java/com/stackroute/service/TrackService.java
@@ -2,30 +2,25 @@
 package com.stackroute.service;
 
 import com.stackroute.domain.Track;
-import exceptions.TrackAlreadyExistExceptions;
-import exceptions.TrackNotFoundExceptions;
 
 import java.util.List;
 
 public interface TrackService {
     //Created abstract method for all.
-    public Track saveTrack(Track track) throws TrackAlreadyExistExceptions;
+    public Track saveTrack(Track track) ;
 
 
-    public Track getTrackById(int id) throws TrackNotFoundExceptions;
+    public Track getTrackById(int id) ;
 
 
     public List<Track> getAllTracks();
 
-    public void deleteTrackById(int id) throws TrackNotFoundExceptions;
-
+    public void deleteTrackById(int id) ;
     public void deleteAllTracks();
 
 
-    public Track updateTrackById(int id, Track track) throws TrackNotFoundExceptions;
-
-    public List<Track> getTrackByName(String trackName) throws TrackNotFoundExceptions;
-
+    public Track updateTrackById(int id, Track track) ;
+    public List<Track> getTrackByName(String trackName) ;
 }
 
 

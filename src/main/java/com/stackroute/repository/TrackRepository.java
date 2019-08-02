3919ea8ac3ada4214f39654c3cfe2b45177c9d1f
Ankita <kumariankita.gaurav@cgi.com>
2019-08-02 14:02:09 +0530

    Done Swagger2 task

--- src/main/java/com/stackroute/repository/TrackRepository.java
+++ src/main/java/com/stackroute/repository/TrackRepository.java
@@ -12,7 +12,7 @@ public interface TrackRepository extends JpaRepository<Track, Integer> {
 
     //    Select * from is not added  and will be added automatically
 //    Ref: https://www.youtube.com/watch?v=WTEGvLXxyOY
-    @Query("select track from Track track where track.trackName like %?1%")
+    @Query("select track from Track track where track.name like %?1%")
     List<Track> searchByName(String Name);
 }
 

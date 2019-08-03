052bd774c140e8f9d6117ead34135b926da93821
Ankita <kumariankita.gaurav@cgi.com>
2019-08-03 15:25:08 +0530

    Completed Lambok task

--- src/main/java/com/stackroute/domain/Track.java
+++ src/main/java/com/stackroute/domain/Track.java
@@ -1,20 +1,28 @@
 package com.stackroute.domain;
 
+import lombok.*;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
 //Entity annotated class will have the ability to represent objects in the database
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Builder
 public class Track {
-    public Track() {
 
-    }
-
-    public Track(int id, String trackName, String comments) {
-        this.id = id;
-        this.trackName = trackName;
-        this.comments = comments;
-    }
+//    public Track() {
+//
+//    }
+//
+//    public Track(int id, String trackName, String comments) {
+//        this.id = id;
+//        this.trackName = trackName;
+//        this.comments = comments;
+//    }
 
     //Id annotation make id variable as Primary key
     @Id
@@ -22,29 +30,29 @@ public class Track {
     private String trackName;
     private String comments;
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getTrackName() {
-        return trackName;
-    }
-
-    public void setTrackName(String trackName) {
-        this.trackName = trackName;
-    }
-
-    public String getComments() {
-        return comments;
-    }
-
-    public void setComments(String comments) {
-        this.comments = comments;
-    }
+//    public int getId() {
+//        return id;
+//    }
+//
+//    public void setId(int id) {
+//        this.id = id;
+//    }
+//
+//    public String getTrackName() {
+//        return trackName;
+//    }
+//
+//    public void setTrackName(String trackName) {
+//        this.trackName = trackName;
+//    }
+//
+//    public String getComments() {
+//        return comments;
+//    }
+//
+//    public void setComments(String comments) {
+//        this.comments = comments;
+//    }
 }
 
 
